a33b2de97a2942290ff62c95c7583fb7817f9fd1
AYESHA <dazyash0@gmail.com>
2026-08-20 20:20:06 +0500

    Time: 0 ms (100.00%) | Memory: 10.5 MB (81.60%) - LeetSync

--- backtracking/79-word-search/word-search.cpp
+++ backtracking/79-word-search/word-search.cpp
@@ -1,42 +1,51 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int rows = board.size();
-        int cols = board[0].size();
-        unordered_set<string> visited;
+        m = board.size(), n = board[0].size();
+        word_sz = word.size();
 
-        auto dfs = [&](int r, int c, int k, auto& dfs) -> bool {
-            if (k == word.length()) {
-                return true;
+        int count[128] = {0};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                count[board[i][j]]++;
             }
-
-            if (r < 0 || r >= rows || c < 0 || c >= cols || visited.count(to_string(r) + "," + to_string(c)) || board[r][c] != word[k]) {
+        }
+        for (char c: word) {
+            if (--count[c] < 0) {
                 return false;
             }
-
-            visited.insert(to_string(r) + "," + to_string(c));
-            bool res = dfs(r + 1, c, k + 1, dfs) || dfs(r - 1, c, k + 1, dfs) || dfs(r, c + 1, k + 1, dfs) || dfs(r, c - 1, k + 1, dfs);
-            visited.erase(to_string(r) + "," + to_string(c));
-            return res;
-        };
-
-        unordered_map<char, int> count;
-        for (char c : word) {
-            count[c]++;
         }
-
         if (count[word[0]] > count[word.back()]) {
             reverse(word.begin(), word.end());
         }
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (dfs(r, c, 0, dfs)) {
-                    return true;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    char tmp = board[i][j];
+                    board[i][j] = '*';
+                    if (backtracking(i, j, 1, board, word)) return true;
+                    board[i][j] = tmp;
                 }
             }
         }
-
-        return false;        
+        return false;
     }
+private:
+    bool backtracking(int i, int j, int index, vector<vector<char>>& board, const string& word) {
+        if (index == word_sz) return true;
+        for (int k = 0; k < 4; k++) {
+            int x = i + dx[k], y = j + dy[k];
+            if (x < 0 || x >= m || y < 0 || y >= n) continue;
+            if (board[x][y] != word[index]) continue;
+            char tmp = board[x][y];
+            board[x][y] = '*';
+            if (backtracking(x, y, index + 1, board, word)) return true;
+            board[x][y] = tmp;
+        }
+        return false;
+    }
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    int m, n, word_sz;
 };
